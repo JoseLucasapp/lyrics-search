11bb17b67bad8266606ee1f14d5cff5a000e0ed3
JoseLucasapp <josluc2016@gmail.com>
2020-12-08 01:01:36 -0300

    add consign and change view engine

--- config/server.js
+++ config/server.js
@@ -6,4 +6,12 @@ const bodyParser = require('body-parser');
 
 let app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
+app.use(bodyParser.json());
+
+consign().include('app/views')
+    .then('app/models')
+    .then('app/controllers')
+
 module.exports = app;
