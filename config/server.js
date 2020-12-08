17a3dfce4e8de4f6c1653a3101843d9370085d87
JoseLucasapp <josluc2016@gmail.com>
2020-12-08 01:14:12 -0300

    add ejs, update server, add controllers, add routes

--- config/server.js
+++ config/server.js
@@ -7,11 +7,13 @@ const bodyParser = require('body-parser');
 let app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'app/views');
+app.set('views', './app/views');
+
+app.use(express.static('app/public'));
 app.use(bodyParser.json());
 
-consign().include('app/views')
-    .then('app/models')
+consign().include('app/models')
     .then('app/controllers')
+    .then('app/routes').into(app);
 
 module.exports = app;
