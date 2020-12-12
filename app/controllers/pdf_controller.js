5ddfe85834af02fef7c94656c9048c7d2baf30b9
JoseLucasapp <josluc2016@gmail.com>
2020-12-12 20:15:59 -0300

    update

--- app/controllers/pdf_controller.js
+++ app/controllers/pdf_controller.js
@@ -4,7 +4,8 @@ module.exports.generate = (application, req, res)=>{
     console.log(body);
     let randomName = Math.floor(Math.random()*10000);
     let doc = application.config.pdfGenerator;
-    doc.text(body.texto, 10,10);
+    doc.text(body.nome, 50,10);
+    doc.text(body.descricao, 10,20);// 10=x 20 =y
     doc.save(`./app/public/pdfs/${randomName}.pdf`);
 
     res.render('index', {validacao: 'Pdf Criado'});
