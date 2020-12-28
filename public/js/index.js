37007df76eec772faf50e61b7faf0f6908a7b112
JoseLucasapp <josluc2016@gmail.com>
2020-12-28 15:47:02 -0300

    add js in code

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector('#search_btn');
+
+btn.addEventListener('click',()=>{
+    api();
+});
+
+api = async()=>{
+    const band = document.querySelector('#band');
+    const music = document.querySelector('#music');
+    const lyrics_space = document.querySelector('#lyrics');
+    const music_space = document.querySelector('#music');
+    lyrics_space.innerHTML='<div id="lyrics">Loading</div>';
+    try{
+        const buscar = await searchLyrics(band.value, music.value);
+        const res = await buscar.json();
+        if(res.lyrics){
+            lyrics_space.innerHTML = res.lyrics;
+            music_space.innerHTML = `Band/Singer: ${band.value} Music: ${music.value}`;
+            band.value = '';
+            music.value = '';
+        }
+        else{
+            lyrics_space.innerHTML = "Check band/singer and song names";
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+searchLyrics = async(band, music)=>{
+    return await fetch(`https://api.lyrics.ovh/v1/${band}/${music}`)
+}
